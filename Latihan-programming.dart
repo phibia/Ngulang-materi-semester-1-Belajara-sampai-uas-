8593925ea3787737cfbe1f5c2a38984b8390e152
darly <se4eiw@gmail.com>
2026-09-18 14:41:43 +0700

    tugas program ganjil genap

---
+++ Latihan-programming.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+  
+  //soal nomor 1
+ void main(){
+
+  print('Selamat Dalam Pemasukan Data Input & Output [Ganjil/Genap]');
+       int data = int.parse(stdin.readLineSync()!);
+
+    if(data % 2 ==0){
+        print('Anda Memasukan Data/Nilai Genap!!');
+    }
+     else{
+         print('Anda Memasukan Data/Nilai Ganjil!!');
+     }
+
+
+    //soal nomor 2
+     
+ }
