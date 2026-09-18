424a0e7b39a710b451112c9d77d063bd572b0a2a
darly <se4eiw@gmail.com>
2026-09-18 14:55:25 +0700

    Soal Kedua i/o postif - ngefatif

--- Latihan-programming.dart
+++ Latihan-programming.dart
@@ -3,17 +3,28 @@ import 'dart:io';
   //soal nomor 1
  void main(){
 
-  print('Selamat Dalam Pemasukan Data Input & Output [Ganjil/Genap]');
-       int data = int.parse(stdin.readLineSync()!);
+//   print('Selamat Dalam Pemasukan Data Input & Output [Ganjil/Genap]');
+//        int data = int.parse(stdin.readLineSync()!);
 
-    if(data % 2 ==0){
-        print('Anda Memasukan Data/Nilai Genap!!');
-    }
-     else{
-         print('Anda Memasukan Data/Nilai Ganjil!!');
-     }
+//     if(data % 2 ==0){
+//         print('Anda Memasukan Data/Nilai Genap!!');
+//     }
+//      else{
+//          print('Anda Memasukan Data/Nilai Ganjil!!');
+//      }
 
 
     //soal nomor 2
-     
+     print("Input angka Anda Disini, apakah [positif/negatif/kodong] : ");
+       int angka = int.parse(stdin.readLineSync()!);
+
+         if(angka > 0) {
+            print("Anda Memasukan Angka Positif!!");
+         }
+          else if(angka < 0) {
+            print("Anda memasukan Angka Negatif!!");
+          }
+           else {
+               print("Anda memasukan Angka Kosong!!");
+          }
  }
